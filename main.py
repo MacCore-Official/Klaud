4ec6dd9a721b48aacb685ab94256ddbfc07000f5
MacCore Official <162847579+MacCore-Official@users.noreply.github.com>
2026-03-05 09:39:43 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from core.bot import KlaudBot
 
+load_dotenv()
+
+async def main():
+    bot = KlaudBot()
+    await bot.start(os.getenv("DISCORD_TOKEN"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
