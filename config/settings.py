fa99fea988b11688dab9efc8fb8d32d79a35c4c8
MacCore Official <162847579+MacCore-Official@users.noreply.github.com>
2026-03-05 14:51:08 +1300

    Delete config directory

--- config/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = int(os.getenv("OWNER_ID", "1269145029943758899"))
